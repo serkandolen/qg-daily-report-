0ee46dbc3b305a36bff978494edcc1c17232d3cb
serkandolen <serkandolen@hotmail.com>
2026-04-17 18:43:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/index.html'
-    }
+  esbuild: {
+    jsx: 'automatic'
   }
 })
